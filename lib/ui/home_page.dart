cb512db54c4d2912e9681e9a008ec6d43993c0a7
tekimbaev <abai.tekimbaev@artware.dev>
2023-03-03 09:42:03 +0600

    first commit

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:dogs/bloc/get_dogs_bloc.dart';
+import 'package:dogs/button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:numberpicker/numberpicker.dart';
+
+class HomePageScreen extends StatefulWidget {
+  const HomePageScreen({super.key});
+
+  @override
+  State<HomePageScreen> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  int _currentVal = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                NumberPicker(
+                    minValue: 1,
+                    maxValue: 50,
+                    value: _currentVal,
+                    onChanged: (val) {
+                      _currentVal = val;
+                      setState(() {});
+                    }),
+                ElevatedButton(
+                  onPressed: () {
+                    BlocProvider.of<GetDogsBloc>(context).add(
+                      GetDataEvent(
+                        count: _currentVal,
+                      ),
+                    );
+                  },
+                  child: const Text('get image'),
+                ),
+                CustomSlidingToggle(
+                    leftLabel: 'Dogs', rightLabel: 'Cats', onChange: (val) {})
+              ],
+            ),
+            const SizedBox(height: 25),
+            Expanded(
+              child: BlocBuilder<GetDogsBloc, GetDogsState>(
+                builder: (context, state) {
+                  if (state is GetDogsSuccess) {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: state.model.message?.length,
+                      itemBuilder: (context, index) => Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          height: 300,
+                          decoration: const BoxDecoration(
+                            color: Colors.green,
+                          ),
+                          child: Image.network(
+                            state.model.message?[index] ?? '',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    );
+                  }
+                  return const SizedBox.shrink();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
