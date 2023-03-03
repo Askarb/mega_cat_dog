bb363acca632b13e1391d9eac4eb4cd211c5972d
tekimbaev <abai.tekimbaev@artware.dev>
2023-03-03 12:22:23 +0600

    toggle

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -14,76 +14,102 @@ class HomePageScreen extends StatefulWidget {
 }
 
 class _HomePageScreenState extends State<HomePageScreen> {
-  int _currentVal = 1;
+  int _currentIndex = 0;
+  List<Widget> pages = [DogsPage(), CatsPage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: CustomSlidingToggle(
-          leftLabel: 'Dogs',
-          rightLabel: 'Cats',
-          onChange: (val) {
-            print(val);
-          },
+        appBar: AppBar(
+          title: CustomSlidingToggle(
+            leftLabel: 'Dogs',
+            rightLabel: 'Cats',
+            onChange: (val) {
+              _currentIndex = val;
+              setState(() {});
+              print(val);
+            },
+          ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                NumberPicker(
-                    minValue: 1,
-                    maxValue: 50,
-                    value: _currentVal,
-                    onChanged: (val) {
-                      _currentVal = val;
-                      setState(() {});
-                    }),
-                ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<GetDogsBloc>(context).add(
-                      GetDataEvent(
-                        count: _currentVal,
-                      ),
-                    );
-                  },
-                  child: const Text('get image'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 25),
-            Expanded(
-              child: BlocBuilder<GetDogsBloc, GetDogsState>(
-                builder: (context, state) {
-                  if (state is GetDogsSuccess) {
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: state.model.message?.length,
-                      itemBuilder: (context, index) => Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          height: 300,
-                          decoration: const BoxDecoration(
-                            color: Colors.green,
-                          ),
-                          child: Image.network(
-                            state.model.message?[index] ?? '',
-                            fit: BoxFit.fill,
-                          ),
+        body: pages[_currentIndex]);
+  }
+}
+
+class DogsPage extends StatefulWidget {
+  const DogsPage({super.key});
+
+  @override
+  State<DogsPage> createState() => _DogsPageState();
+}
+
+class _DogsPageState extends State<DogsPage> {
+  int _currentVal = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              NumberPicker(
+                  minValue: 1,
+                  maxValue: 50,
+                  value: _currentVal,
+                  onChanged: (val) {
+                    _currentVal = val;
+                    setState(() {});
+                  }),
+              ElevatedButton(
+                onPressed: () {
+                  BlocProvider.of<GetDogsBloc>(context).add(
+                    GetDataEvent(
+                      count: _currentVal,
+                    ),
+                  );
+                },
+                child: const Text('get image'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 25),
+          Expanded(
+            child: BlocBuilder<GetDogsBloc, GetDogsState>(
+              builder: (context, state) {
+                if (state is GetDogsSuccess) {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: state.model.message?.length,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        height: 300,
+                        decoration: const BoxDecoration(
+                          color: Colors.green,
+                        ),
+                        child: Image.network(
+                          state.model.message?[index] ?? '',
+                          fit: BoxFit.fill,
                         ),
                       ),
-                    );
-                  }
-                  return const SizedBox.shrink();
-                },
-              ),
+                    ),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class CatsPage extends StatelessWidget {
+  const CatsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
